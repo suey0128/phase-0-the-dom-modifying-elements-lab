fd2001c9ce9a05adfd444f5e0077a6e24d64a894
suey0128 <syui0128@gmail.com>
2021-04-27 23:06:01 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+
+newHeader.innerHTML = "OUR-NAME is the champion"
+
